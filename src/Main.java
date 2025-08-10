9206afc1933b67c6134804f904123e21f23eaf3f
krishna koumudi <koumudi26@gmail.com>
2025-08-09 21:22:31 -0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        double betAmount = 20.0;
+        double betAmount = 30.0;
 
         // Initialize payout table
         Map<Symbol, int[]> payoutTable = new HashMap<>();
